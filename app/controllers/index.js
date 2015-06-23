c6712fac242b4c48210c88f67f7276cb176a719f
Fernanda Pessôa <feerpessoa.j@hotmail.com>
2015-06-23 01:48:29 -0300

    persistindo no mongodb :DDD #partiucama

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,20 @@
+var MongoClient = require('mongodb').MongoClient;
+ 
+var myCollection;
+var db = MongoClient.connect('mongodb://127.0.0.1:27017/filipefofinho', function(err, db) {
+    if(err)
+        throw err;
+    console.log("Conectado com mongoDB!");
+    myCollection = db.collection('username');
+});
+
+
 module.exports = function( app ) {
     var controller = {
 
     direcionarIndex : function (request, response)
     {
-    	response.render("index");
+    response.render("index");
     },
 
     direcionarHello : function (request, response)
@@ -15,35 +26,43 @@ module.exports = function( app ) {
 
     direcionarMeuNome : function (request, response) 
     {
-    	response.render("index");
+    response.render("index");
     },
 
     meuNome : function (request, response)
     {
-    	var nome = request.body.nome;
-    	response.render("index", {
-    		"nome" : nome
-    	});
+    var nome = request.body.nome;
+    response.render("index", {
+    "nome" : nome
+    }); 
+
+        myCollection.insert({nome: nome}, function(err, result) {
+            if(err)
+                throw err;
+         
+            console.log("********* " + nome + ", salvo com sucesso!!! *********");
+        });     
+
     },
 
     direcionarSoma : function (request, response)
     {
-    	response.render("somar");
+    response.render("somar");
     },
 
     somar : function (request, response) {
-    	var num1 = parseInt(request.body.num1);
-    	var num2 = parseInt(request.body.num2);
+    var num1 = parseInt(request.body.num1);
+    var num2 = parseInt(request.body.num2);
 
-    	var resultado = controller.somar2Numeros (num1, num2);
+    var resultado = controller.somar2Numeros (num1, num2);
 
-    	response.render("somar", {
-    		"resultado" : resultado
-    	});
+    response.render("somar", {
+    "resultado" : resultado
+    });
     },
 
     somar2Numeros : function (num1, num2) {
-    	return num1 + num2;
+    return num1 + num2;
     },
 
     direcionarSubtracao : function (request, response)
