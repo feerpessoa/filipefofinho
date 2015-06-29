8fb59a6a62de38e4e74f28e8d87039d46234d159
feerpessoa.j@hotmail.com <fernandapessoa@MacBook-Air-de-Fernanda.local>
2015-06-29 10:30:49 -0300

    removendo aws sdk

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,26 +10,6 @@ var db = MongoClient.connect('mongodb://filipefofinho:12345678@ds047800.mongolab
     myCollection = db.collection('username');
 });
 
-var AWS = require('aws-sdk');
-
-var s3 = new AWS.S3();
-
-s3.createBucket({Bucket: 'myBucket'}, function() {
-
-  var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
-
-  s3.putObject(params, function(err, data) {
-
-      if (err)
-
-          console.log(err)
-
-      else console.log("Successfully uploaded data to myBucket/myKey");
-
-   });
-
-});
-
 
 module.exports = function( app ) {
     var controller = {
